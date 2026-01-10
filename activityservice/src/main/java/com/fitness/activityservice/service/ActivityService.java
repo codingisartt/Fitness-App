3012cbad1c0bb43a2ef73ecee1da3d9a900b8692
Tubanur Balci <tubanurbalci5015@gmail.com>
2026-01-10 20:14:24 +0300

    Add user validation via Eureka-based inter-service communication

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -17,7 +17,14 @@ public class ActivityService {
 
     private final ActivityRepository activityRepository;
 
+    private final UserValidationService userValidationService;
+
     public ActivityResponse trackActivity(ActivityRequest request) {
+
+        boolean isValidUser = userValidationService.validateUser(request.getUserId());
+        if (!isValidUser) {
+            throw new RuntimeException("Invalid user: " + request.getUserId());
+        }
         Activity activity = Activity.builder()
                 .userId(request.getUserId())
                 .type(ActivityType.valueOf(request.getType()))
